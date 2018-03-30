e7a65162b047057ea8ff0efdc03e95ee21215ec3
Anne Starley <annestarley@gmail.com>
2018-03-29 17:45:38 -0700

    tryna fix token and stuff

--- controller/tokens.js
+++ controller/tokens.js
@@ -2,28 +2,19 @@ const model = require('../models/users.js');
 const jwt = require('jsonwebtoken');
 
 const tokenVerify = (req, res, next)=>{
-  req.uid=1;//TODO FIX
-  console.log(x);
-  next(req,res);
-
-
-    let auth= req.headers.auth;
-    if(!auth)
-    {
-      return res.status(403).send(`token denied`);
-    }
-    else {
-      auth = auth.split(' ')[1]
-      jwt.VerifyAsync(auth, process.env.TOKEN_SECRET)
-      .then(x=>{
-        req.uid=1;//TODO FIX
-        console.log(x);
-        next(req,res);
-      })
-      .catch(err=>{
-        console.log(err)
-        res.status(403).send(`there was an error`)
-      });
+  let auth= req.headers.auth;
+  if(!auth)
+  {
+    return res.status(403).send(`token denied`);
+  }
+  else {
+    auth = auth.split(' ')[1]
+    let x= jwt.Verify(auth, 'shhhhh')
+      if(x)
+      {
+        return next(req,res);
+      }
+      res.status(403).send(`there was an error`)
   }
 }
 
